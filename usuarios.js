0a31b3ae134a1de694ae2c4c7d6f6d8fd962ef43
matias-kreig <matiaskreig1995@gmail.com>
2022-07-26 16:57:12 -0300

    Segundo Desafio Entregable

---
+++ usuarios.js
@@ -0,0 +1,25 @@
+/* class Ingreso{
+    
+    constructor(usuarioIngresar, nombreIngresar){
+    
+    this.usuarioIngresar = usuarioIngresar;
+    
+    this.nombreIngresar = nombreIngresar;
+    
+  }
+  
+  
+    
+}
+
+const usuario1 = new Ingreso("MatiasK95", "Matias" );
+const usuario2 = new Ingreso("PepitoR75", "Pepito" );
+const usuario3 = new Ingreso("VanesaH80", "Vanesa" );
+
+
+const usuarios = [];
+
+const totalUsuarios = usuarios.push(usuario1,usuario2,usuario3);
+
+
+ */
